158abcd7af9762ac74cb54d56193a5f3aafe120f
Rob Pascarella <robpascarella@Robs-iMac.local>
2020-08-16 17:23:38 -0500

    P/L tracker in place

--- PyBank/Analysis/main.py
+++ PyBank/Analysis/main.py
@@ -7,6 +7,11 @@
 import os
 import csv
 
+# print a header for table cleanliness 
+print("-"*27)
+print("Financial Analysis")
+print("-"*27)
+
 
 # import the csv file 
 csvpath = os.path.join('..', 'Resources', 'budget_data.csv')
@@ -22,15 +27,28 @@ with open(csvpath, 'r') as csvfile:
  # 
  # I see there are two columns, and that the first row is a header titles 'Date' & 'Profit / Loss'    
  # Knowing that the first row is a header and seeing that the column1 data is monthly,
- # the total number of    
-    
+ # the total number of months in the data:
+  
+    csv_header = next(csv_reader)  # skips header in the count.
     data = list(csv_reader)
-    row_count = len(data)
-    num_months = row_count - 1   
-    print(f'Total Months = {num_months}')
+    row_count = len(data) 
+    print(f'Total Months : {row_count}')
+
+
+# total P/L for the dataset is the summation of that column 
+
+with open(csvpath, 'r') as csvfile:
+    csv_reader = csv.reader(csvfile, delimiter = ',')
+    csv_header = next(csv_reader)  # again skipping header for the calc
+    total_pl = 0
+    for row in csv_reader: 
+        total_pl += float(row[1])
+    print(f'Total P/L : ${total_pl}')
 
 
 ######## --- csvreader.__next__()
+
+
 
 
 ## analyze the total P/L for the entire dataset 
