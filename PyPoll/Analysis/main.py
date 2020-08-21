c805d1b0bb05dadd04b4dbbf26ca496a839bc3a5
Rob Pascarella <robpascarella@Robs-iMac.local>
2020-08-21 12:57:07 -0500

    post-tutor session: she helepd answer a lot of questions. needs cleaned up.

--- PyPoll/Analysis/main.py
+++ PyPoll/Analysis/main.py
@@ -24,44 +24,48 @@ with open(csvpath, 'r') as csvfile:
        #     break
 
     csv_header = next(csv_reader)
-    data = list(csv_reader)
-    row_count = len(data)
+    # data = list(csv_reader)
+    # row_count = len(data)
+
+
+        #or move these up above 18
+    candidates = []
+    Khan = []
+    Correy = []
+    Li = []
+    OTooley = []
+    row_count = 0
+    
+        ## with open(csvpath, 'r') as csvfile:
+        ##    csv_reader = csv.reader(csvfile, delimiter = ',')
+    for row in csv_reader:
+     #   print(row)
+        candidates.append(row[2])
+        row_count += 1 
+
     print(f'Total Votes: {row_count}')
 
     print("-"*27)
-
-
-candidates = []
-Khan = []
-Correy = []
-Li = []
-OTooley = []
-
-with open(csvpath, 'r') as csvfile:
-    csv_reader = csv.reader(csvfile, delimiter = ',')
-    for row in csv_reader:
-        candidates.append(row[2])
 
     for row in candidates:
         if row == "Khan":
             Khan.append(row[0])
             Total_Khan = len(Khan)
             Khan_percentage = (Total_Khan / row_count)*100
-    
   
-    for row in candidates:   
+           #   for row in candidates:   
         if row == "Correy":
             Correy.append(row[0])
             Total_Correy = len(Correy)
             Correy_percentage = (Total_Correy / row_count)*100
         
-    for row in candidates:
+         #  for row in candidates:
         if row == "Li":
             Li.append(row[0])
             Total_Li = len(Li)
             Li_percentage = (Total_Li / row_count)*100
       
-    for row in candidates:
+            #   for row in candidates:
         if row == "O'Tooley":
             OTooley.append(row[0])
             Total_OTooley = len(OTooley)
@@ -71,19 +75,27 @@ with open(csvpath, 'r') as csvfile:
     winner = max(totals)
 
 
-    print(f'Khan: {Khan_percentage}% ({Total_Khan})')
+    print(f'Khan: {"{:,.2f}".format(Khan_percentage)}% ({Total_Khan})')
     print(f'Correy: {Correy_percentage}% ({Total_Correy})')
     print(f'Li: {Li_percentage}% ({Total_Li})')
     print(f'OTooley: {OTooley_percentage}% ({Total_OTooley})')
 
     print("-"*27)
 
-    print(f'Winner: {winner}')
+    if winner == Total_Khan:
+        print("Winner: Khan")
+    if winner == Total_Correy:
+        print("Winner: Correy")
+    if winner == Total_Li:
+        print("Winner: Li")
+    if winner == Total_OTooley:
+        print("Winner: OTooley")
 
     print("-"*27)
 
-### now need to work export to txt file and printing winner 
+### now need to work export to txt file  AND formatting % 
 
+## with open(outpath, 'w') as csvfile:
    
 
 
